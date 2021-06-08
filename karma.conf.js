451ecee40e0278590097b69a001f034897bbe355
varu4300 <minesh.varu@gmail.com>
2021-06-08 19:57:37 -0400

    updating unit test config
    - Updating karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -3,14 +3,6 @@
 
 module.exports = function(config) {
     config.set({
-        check: {
-            global: {
-                statements: 80,
-                branches: 80,
-                functions: 80,
-                lines: 80
-            }
-        },
         basePath: '',
         frameworks: ['jasmine', '@angular-devkit/build-angular'],
         plugins: [
